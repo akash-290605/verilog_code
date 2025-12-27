4ca20b1de85e5cf4f0e7c0f4556dbc233ead3c73
akash-290605 <akashkarthikeyan29@gmail.com>
2025-12-27 17:10:23 +0530

    Add files via upload

---
+++ 4X1mux/mux_tb.v
@@ -0,0 +1,17 @@
+`include "Mux4_1.v"
+module top;
+reg [1:0]s;
+reg [3:0]i;
+wire y;
+
+Mux4_1 dut(y,s,i);
+initial begin
+repeat(10) begin
+s=$random();
+i=$random();
+#10;
+
+$display("time=%0t |input=%b | selectline=%b | output=%b",$time,i,s,y);
+end
+end
+endmodule
